e18cc6bddb74fd06c1f9a43ce877c63cf2f72c5e
Andrew Olney <aolney@memphis.edu>
2023-06-05 17:42:00 -0500

    basic version but without endpoint set up or tested

---
+++ src/StarChatAPI.fs
@@ -0,0 +1,28 @@
+module StarChatAPI
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Thoth.Json 
+open Thoth.Fetch
+
+let mutable endpointOption : string option = None
+
+type CodingAssistanceRequest =
+    {
+        user_message : string
+    }
+
+type CodingAssistanceResponse=
+    {
+        bot_response : string
+    }
+///Get coreferences from AllenNLP
+let SendMessage( input: string ) : JS.Promise<Result<CodingAssistanceResponse,FetchError>> =
+    let endpoint = 
+        match endpointOption with
+        | Some( endpoint ) -> endpoint
+        | None -> "TODO TEST ENDPOINT"
+    promise {
+        return! Fetch.tryPost( endpoint , { user_message = input }, caseStrategy = SnakeCase)
+}
--- src/index.fs
+++ src/index.fs
@@ -4,45 +4,101 @@ open Fable.Core
 open Fable.Core.JS
 open Fable.Core.JsInterop
 open Browser.Dom
+open Browser.Types
 open Import.Jupyter
 
 //sugar for creating js objects
 let inline (!!) x = createObj x
 let inline (=>) x y = x ==> y
 
-//TODO: hack b/c I haven't dug through the imports to understand the `requires` semantics
 [<ImportMember("@jupyterlab/apputils")>]
 let ICommandPalette : obj = jsNative
 
+//Some UI elements are declared outside the extension object so we can refer to them in functions
+//This is a lightweight alternative to using class syntax on the extension
+
+let history_div = Browser.Dom.document.createElement("div")
+history_div.setAttribute("style","width: 100%; max-width: 100%;height:440px;overflow: auto;")
+history_div.id <- "history_div"
+let user_input = Browser.Dom.document.createElement("textarea")
+user_input.setAttribute("style","width: 100%; max-width: 100%")
+user_input.id <- "user_input"
+
+let appendResponseToHistory( response ) =
+    let newDiv = Browser.Dom.document.createElement("div")
+    newDiv.innerHTML <- response
+    newDiv.setAttribute("style","color:LemonChiffon;") //"width: 100%; max-width: 100%;height:440px;overflow: auto;")
+    // newDiv.id <- "history_div"
+    history_div.appendChild(newDiv) |> ignore
+
+let appendErrorToHistory( ) =
+    let newDiv = Browser.Dom.document.createElement("div")
+    newDiv.innerHTML <- "An error occurred when contacting StarChat. See your browser console for more information."
+    newDiv.setAttribute("style","color:Tomato;") //"width: 100%; max-width: 100%;height:440px;overflow: auto;")
+    // newDiv.id <- "history_div"
+    history_div.appendChild(newDiv) |> ignore
+
+let sendUserInput() =
+    // get  user message from textarea
+    let user_message = user_input.innerText
+
+    // send to API, get bot response
+    promise {
+        let! coding_assistance_response = StarChatAPI.SendMessage user_message
+        match coding_assistance_response with
+        | Ok( ok ) ->
+            //log
+            Logging.LogToServer( Logging.StarChatLogEntry060623.Create user_message ok.bot_response )
+
+            //update UI
+            appendResponseToHistory(ok.bot_response)
+
+        | Error( e ) ->
+            //update UI with the error
+            appendErrorToHistory()
+            console.log(e)
+    }
+    // ()
+
 let extension =
     !! [
-        "id" => "jupyterlab_fable_extension"
+        "id" => "jupyterlab_starchat_extension"
         "autoStart" => true
         "requires" => [| ICommandPalette |] 
         //------------------------------------------------------------------------------------------------------------
         //NOTE: this **must** be wrapped in a Func, otherwise the arguments are tupled and Jupyter doesn't expect that
         //------------------------------------------------------------------------------------------------------------
         "activate" =>  System.Func<JupyterFrontEnd,ICommandPalette,unit>( fun app palette ->
-            console.log("JupyterLab extension jupyterlab_fable_extension is activated!");
+            console.log("JupyterLab extension jupyterlab_starchat_extension is activated!");
             // Create a blank content widget inside of a MainAreaWidget
             let content = PhosphorWidgets.Widget.Create();
             let widget = JupyterlabApputils.MainAreaWidget.Create( !![ "content" => content ]);
-            widget.id <- "apod-jupyterlab";
-            widget.title.label <- "Fable Demo";
+            widget.id <- "jupyterlab_starchat_extension";
+            widget.title.label <- "StarChat Coding Assistant";
             widget.title.closable <- Some(true);
 
-            // Add html text to the widget
-            let p = Browser.Dom.document.createElement("p")
-            p.setAttribute("style","font-size:72px")
-            p.innerText <- "Hello from Fable"
-            content.node.appendChild(p) |> ignore
+            // Add chat history to widget
+            //history_div.innerText <- "Hello from Fable"
+            content.node.appendChild(history_div) |> ignore
+
+            // Add user input to widget
+            content.node.appendChild(user_input) |> ignore
+
+            // Add send button to widget
+            let send_button = document.createElement ("button")
+            send_button.setAttribute("style","width: 100%; max-width: 100%")
+            send_button.id <- "send_button"
+            send_button.innerText <- "Send"
+            send_button.addEventListener ("click", (fun _ -> sendUserInput() |> ignore ))
+            content.node.appendChild(send_button) |> ignore
+
 
             // Add application command
-            let command = "apod:open"
+            let command = "jupyterlab_starchat_extension:open"
             //TODO: using dynamic (?) b/c the imports aren't fully implemented
             app.commands?addCommand( command, 
                 !![
-                    "label" => "Fable Jupyterlab Extension"
+                    "label" => "StarChat"
                     "execute" => fun () -> 
                         if not <| widget.isAttached then
                             app.shell?add(widget, "main")
@@ -52,9 +108,28 @@ let extension =
             palette?addItem(
                 !![
                     "command" => command
-                    "category" => "Fable"
+                    "category" => "Coding Assistants"
                 ]
             )
+
+            let searchParams = Browser.Url.URLSearchParams.Create(  Browser.Dom.window.location.search )
+            // Browser.
+                            
+            //If query string has id=xxx, store this identifier as a participant id
+            match searchParams.get("id") with
+            | Some(id) -> Logging.idOption <- Some(id)
+            | _ -> ()
+
+            //If query string has log=xxx, use this at the logging endpoint
+            //must include http/https in url
+            match searchParams.get("log") with
+            | Some(logUrl) -> Logging.logUrl <- Some(logUrl)
+            | _ -> ()
+
+            //get the api endpoint from the query string
+            match searchParams.get("endpoint") with
+            | Some(endpoint: string) -> StarChatAPI.endpointOption <- Some(endpoint)
+            | _ -> ()
         )
  
     ]
